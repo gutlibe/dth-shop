1cc3cbef2e2ec1de7baab5d53f257bcb9f2e1cf4
gutlibe <119524672+gutlibe@users.noreply.github.com>
2024-11-09 05:03:36 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Constants and Configuration
 const KEYS = {
-  PAYSTACK_PUBLIC_KEY: "pk_test_704cf54438a379b4e0458da22ad5a56159d0bb62",
+  PAYSTACK_PUBLIC_KEY: "pk_live_2e7c8c499640fe32483a9c41ab85af3385ef8c87",
   TELEGRAM_BOT_TOKEN: "7844946740:AAGvoS-8ziX5aKDy9Gw5edPqqsiCxgfQxs8",
   TELEGRAM_CHAT_ID: "8070411940"
 };
